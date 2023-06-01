1533c9e8017c0e8aa79eb0f4fd5d9f380bb3d66a
Will Chen <wfchen1990@gmail.com>
2023-06-01 08:18:59 -0600

    Update file

--- each.rb
+++ each.rb
@@ -5,10 +5,18 @@
 # Challenge 1
 # Use the each method to print out a complete sentence stating the weather using the Array below:
 weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+weather_options.each do|weather|
+  puts "Today's forecast is #{weather}!"
+end
+
 
 # Challenge 2
 # Use a ruby method to organize the names in alphabetical order
 # Then use the each method to print out a greeting for each person
 names = ["Janet", "Alfonso", "Aaron", "Stephanie", "Tanner", "Robert", "Justin"]
+names.sort!
+names.each do |name|
+  puts "Hello, #{name}!"
+end
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
